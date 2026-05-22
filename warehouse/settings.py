17f2947a5860194bd38cc9c2c80d435d2b29b187
unknown <ahimanajeanlouis@gmail.com>
2026-05-22 17:23:40 +0200

    fix static files

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -80,13 +80,19 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 # =========================
 # DATABASE (IMPORTANT FIX)
 # =========================
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # =========================
 # PASSWORD VALIDATION
